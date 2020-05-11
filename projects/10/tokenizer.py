f07bb876f110263a514f4337eb31bdd43c8a4070
rcpd <rcpd22@gmail.com>
2020-05-12 08:34:38 +1000

    tokenizer: all T tests passed

--- projects/10/tokenizer.py
+++ projects/10/tokenizer.py
@@ -1,8 +1,6 @@
 
 import re
 
-# TODO: TODO: integerConstant, symbol escaping
-
 
 def parse(word, keyword, strings, output):
     keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean',
@@ -18,8 +16,8 @@ def parse(word, keyword, strings, output):
 
     # process symbols that have been parsed to single word
     if word in symbols:
-        print("<symbol> %s </symbol>" % word.replace("<", "&lt;").replace(">", "&gt;"))
-        output.append("<symbol> %s </symbol>" % word.replace("<", "&lt;").replace(">", "&gt;"))
+        print("<symbol> %s </symbol>" % word.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;"))
+        output.append("<symbol> %s </symbol>" % word.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;"))
         if word == ";":
             keyword = False  # disable once identifier(s) processed
         return output, keyword
@@ -60,60 +58,70 @@ def parse(word, keyword, strings, output):
 
 
 def main():
-    filepath = r"ArrayTest\Main.jack"
+    jack_filepaths = [
+        r"ArrayTest\Main.jack",
+        r"ExpressionLessSquare\Main.jack",
+        r"ExpressionLessSquare\Square.jack",
+        r"ExpressionLessSquare\SquareGame.jack",
+        r"Square\Main.jack",
+        r"Square\Square.jack",
+        r"Square\SquareGame.jack"
+    ]
 
-    with open(filepath) as jack_file:
-        jack_content = jack_file.readlines()
+    for filepath in jack_filepaths:
+        with open(filepath) as jack_file:
+            jack_content = jack_file.readlines()
 
-    block_comment = False
-    output = []
-    print("<tokens>")
-    output.append("<tokens>")
-    strings = {}
-    string_id = 0
+        block_comment = False
+        output = []
+        print("<tokens>")
+        output.append("<tokens>")
+        strings = {}
+        string_id = 0
 
-    for line in jack_content:
-        if block_comment:
-            continue
+        for line in jack_content:
+            line = line.strip()
+            if line == "" or line.startswith("//"):
+                continue
+            elif "/*" in line:
+                if "*/" not in line:
+                    block_comment = True
+                continue
+            elif "*/" in line:
+                block_comment = False
+                continue
 
-        line = line.strip()
-        if line == "" or line.startswith("//"):
-            continue
-        elif "/*" in line:
-            if "*/" not in line:
-                block_comment = True
-            continue
-        elif "*/" in line:
-            block_comment = False
-            continue
+            if block_comment:
+                continue
 
-        print("// line:", line)
+            print("// line:", line)
+            line = line.split("//")[0].strip()
 
-        # string_constant parsing
-        if '"' in line:
-            string_line = line.split('"')
-            for i in range(0, len(string_line), 3):
-                substring = string_line[i+1]
-                strings["__string%s__" % string_id] = substring
-                line = line.replace(substring, " __string%s__ " % string_id)
-                string_id += 1
+            # string_constant parsing
+            if '"' in line:
+                string_line = line.split('"')
+                for i in range(0, len(string_line), 3):
+                    substring = string_line[i+1]
+                    strings["__string%s__" % string_id] = substring
+                    line = line.replace(substring, " __string%s__ " % string_id)
+                    string_id += 1
 
-            while '"' in line:
-                line = line.replace('"', '')
-            print("// line post-replace:", line)
+                while '"' in line:
+                    line = line.replace('"', '')
+                print("// line post-replace:", line)
 
-        words = line.split()
-        keyword = False
-        print("// words:", words)
+            words = line.split()
+            keyword = False
+            print("// words:", words)
 
-        for word in words:
-            output, keyword = parse(word, keyword, strings, output)
+            for word in words:
+                output, keyword = parse(word, keyword, strings, output)
 
-    print("</tokens>")
-    output.append("</tokens>")
-    with open(filepath.replace(".jack", "T_out.xml"), "w") as output_file:
-        for line in output:
-            output_file.write(line + "\n")
+        print("</tokens>")
+        output.append("</tokens>")
+        with open(filepath.replace(".jack", "T_out.xml"), "w") as output_file:
+            for line in output:
+                output_file.write(line + "\n")
 
 
 if __name__ == '__main__':
