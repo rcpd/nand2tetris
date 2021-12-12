1070304b0bbde87a40f5d826b704485bf5d4f465
rcpd <rcpd22@gmail.com>
2021-12-12 14:30:09 +1100

    centralize python engines

--- projects/11/compile.bat
+++
@@ -1,3 +0,0 @@
-C:\dev\nand2tetris\venv\scripts\python.exe C:\dev\nand2tetris\projects\11\tokenizer.py
-C:\dev\nand2tetris\venv\scripts\python.exe C:\dev\nand2tetris\projects\11\analyzer.py
-C:\dev\nand2tetris\venv\scripts\python.exe C:\dev\nand2tetris\projects\11\compiler.py
