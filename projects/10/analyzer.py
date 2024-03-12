bcc0297427d21101e012cc0960be1384971af80e
rcpd <5835162+rcpd@users.noreply.github.com>
2024-03-12 20:53:58 +1100

    analyzer: subroutineDec, parameterList

--- projects/10/analyzer.py
+++ projects/10/analyzer.py
@@ -15,27 +15,66 @@ def main(debug=False):
     ]
 
     output = []
+
     for filepath in jack_filepaths:
         input_tree = ET.parse(filepath.replace(".jack", "T_out.xml"))
         input_root = input_tree.getroot()
+        output_root = ET.Element("class")
+        input_list = []
+
+        # read the token XML into something else more easily traversed
         for input_child in input_root:
             if input_child.tag == "tokens":
                 continue
             element = input_child.text.strip()
 
-            if debug:
-                print("// line:", input_child.tag, element)
+            # if debug:
+            #     print("// line:", input_child.tag, element)
+            input_list.append((input_child.tag, element))
 
-        # initialize root
-        output_root = None
-        # check if current element is first written
-        if output_root is None:
-            parent = output_root = ET.Element("element")
-        else:
-            parent = ET.SubElement(output_root, "element")
+        # process the token stream
+        prev_parent = parent = output_root
+        for i, input_tuple in enumerate(input_list):
+            j = i+1  # next token
 
-        child = ET.SubElement(parent, "child")
-        child.text = "child_text"
+            # function def (+1)
+            if j < len(input_list):
+                if parent.tag == "class" and input_list[i][0] == "symbol" and input_list[i][1] == "{":
+                    # insert current token
+                    child = ET.SubElement(parent, input_tuple[0])
+                    child.text = " %s " % input_tuple[1]
+
+                    # then inject new token & update parent
+                    if debug:
+                        print("// insert:", "subroutineDec")
+                    child = ET.SubElement(parent, "subroutineDec")
+                    prev_parent = parent
+                    parent = child
+
+                # populate parameterList for function def
+                elif parent.tag == "subroutineDec" and input_list[i][0] == "symbol" and input_list[i][1] == "(":
+                    # insert current token
+                    child = ET.SubElement(parent, input_tuple[0])
+                    child.text = " %s " % input_tuple[1]
+
+                    # then inject new token & update parent
+                    if debug:
+                        print("// insert:", "parameterList")
+                    child = ET.SubElement(parent, "parameterList")
+                    prev_parent = parent
+                    parent = child
+
+                # close parameterList for function def
+                elif parent.tag == "parameterList" and input_list[i][0] == "symbol" and input_list[i][1] == ")":
+                    # revert parent then insert current token
+                    parent = prev_parent
+                    child = ET.SubElement(parent, input_tuple[0])
+                    child.text = " %s " % input_tuple[1]
+                    
+                # insert current token
+                else:
+                    child = ET.SubElement(parent, input_tuple[0])
+                    child.text = " %s " % input_tuple[1]
 
         # write output
         tree_string = ET.tostring(output_root)
@@ -46,8 +85,8 @@ def main(debug=False):
         if debug:
             print("\n%s" % pretty_xml)
 
-        # with open(output_filepath, "w") as output_file:
-        #     output_file.write(pretty_xml)
+        with open(output_filepath, "w") as output_file:
+            output_file.write(pretty_xml)
 
 
 if __name__ == '__main__':
