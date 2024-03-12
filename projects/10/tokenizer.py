18a596ce33f1b0cf1de6615d0c6125735b7de834
rcpd <5835162+rcpd@users.noreply.github.com>
2024-03-12 20:53:57 +1100

    tokenizer: stringConstant, symbol recursion

--- projects/10/tokenizer.py
+++ projects/10/tokenizer.py
@@ -1,60 +1,117 @@
 
-# chars = "abcdefghijklmnopqrstuvwxyz"
+import re
+
+# TODO: TODO: integerConstant, symbol escaping
 
 
-filepath = r"ArrayTest\Main.jack"
+def parse(word, keyword, strings, output):
+    keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean',
+                'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
+    symbols = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~', '"']
+    types = ["Array"]
 
-with open(filepath) as jack_file:
-    jack_content = jack_file.readlines()
+    # process string_constant placeholders
+    if re.match(r"__string\d+__", word):
+        print("<stringConstant> %s </stringConstant>" % strings[word])
+        output.append("<stringConstant> %s </stringConstant>" % strings[word])
+        return output, keyword
 
-block_comment = False
-keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void',
-            'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
-symbols = ['{', '}', '(', ')', '[', ']', '. ', ', ', '; ', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+    # process symbols that have been parsed to single word
+    if word in symbols:
+        print("<symbol> %s </symbol>" % word)
+        output.append("<symbol> %s </symbol>" % word)
+        if word == ";":
+            keyword = False  # disable once identifier(s) processed
+        return output, keyword
 
-print("<tokens>")
-
-for line in jack_content:
-    if block_comment:
-        continue
-
-    line = line.strip()
-    if line == "" or line.startswith("//"):
-        continue
-    elif "/*" in line:
-        if "*/" not in line:
-            block_comment = True
-        continue
-    elif "*/" in line:
-        block_comment = False
-        continue
-
-    words = line.split()
-    keyword = False
-    print(line, "-----", words)
-    for i, word in enumerate(words):
-        # search for symbols in word
-        symbol = False
-        for j, char in enumerate(word):
+    # search for symbols in word
+    else:
+        for i, char in enumerate(word):
             if char in symbols:
-                symbol = char
-                if word[:j]:  # if something left after slice
-                    words.insert(i+1, word[j+1:])  # inject symbol (and remainder) into next word
-                word = word[:j]  # truncate word to before symbol char
-                break
+                if i == 0:  # can't slice [0:0]
+                    output, keyword = parse(word[0], keyword, strings, output)  # process symbol
+                    output, keyword = parse(word[1:], keyword, strings, output)
+                else:
+                    if word[:i]:
+                        output, keyword = parse(word[:i], keyword, strings, output)  # process word prior to symbol
+                        word = word[i:]  # truncate word to symbol char onwards
+                        output, keyword = parse(word[0], keyword, strings, output)  # process symbol
+                    if word[1:]:  # slices return '' if out of bounds
+                        output, keyword = parse(word[1:], keyword, strings, output)  # any remaining word data
+                return output, keyword
 
-        if keyword and word not in keywords:
+    # process regular words (no symbols)
+    if word:  # word might be '' after slicing
+        if word in types:
             print("<identifier> %s </identifier>" % word)
-            keyword = False  # disable once identifier processed
+            output.append("<identifier> %s </identifier>" % word)
+        elif keyword and word not in keywords:
+            print("<identifier> %s </identifier>" % word)
+            output.append("<identifier> %s </identifier>" % word)
         elif word in keywords:
             print("<keyword> %s </keyword>" % word)
-            keyword = True  # set until corresponding identifier processed
+            output.append("<keyword> %s </keyword>" % word)
+            keyword = word  # set until corresponding identifier processed
 
-        if symbol:  # not mutually exclusive with previous
-            print("<symbol> %s </symbol>" % symbol)
-            symbol = False
-
-print("</tokens>")
+    return output, keyword
 
 
+def main():
+    filepath = r"ArrayTest\Main.jack"
 
+    with open(filepath) as jack_file:
+        jack_content = jack_file.readlines()
+
+    block_comment = False
+    output = []
+    print("<tokens>")
+    output.append("<tokens>")
+    strings = {}
+    string_id = 0
+
+    for line in jack_content:
+        if block_comment:
+            continue
+
+        line = line.strip()
+        if line == "" or line.startswith("//"):
+            continue
+        elif "/*" in line:
+            if "*/" not in line:
+                block_comment = True
+            continue
+        elif "*/" in line:
+            block_comment = False
+            continue
+
+        print("// line:", line)
+
+        # string_constant parsing
+        if '"' in line:
+            string_line = line.split('"')
+            for i in range(0, len(string_line), 3):
+                substring = string_line[i+1]
+                strings["__string%s__" % string_id] = substring
+                line = line.replace(substring, " __string%s__ " % string_id)
+                string_id += 1
+
+            while '"' in line:
+                line = line.replace('"', '')
+            print("// line post-replace:", line)
+
+        words = line.split()
+        keyword = False
+        print("// words:", words)
+
+        for word in words:
+            output, keyword = parse(word, keyword, strings, output)
+
+    print("</tokens>")
+    output.append("</tokens>")
+    with open(filepath.replace(".jack", "T_out.xml"), "w") as output_file:
+        for line in output:
+            output_file.write(line + "\n")
+
+
+if __name__ == '__main__':
+    main()
